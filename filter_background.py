03d4ccf59b316ca3e09b4175d18fad45884c79fb
WYLee <WY.Lee@auo.com>
2023-05-04 11:29:30 +0800

    0504

--- filter_background.py
+++ filter_background.py
@@ -3,19 +3,33 @@ import numpy as np
 import os
 
 
-### 讀取圖片
-def mask(img):
+def background(folder_path, fileName):
+    img = cv2.imread('{}/{}.jpg'.format(folder_path, fileName))
     imgOrgi = img.copy()
-    # img = cv2.resize(img, (640,480))
     height, width, _ = img.shape
-    img = cv2.resize(img, (width//4, height//4))
+    resizeValue = 4
+    paddingValue = 10
+    img = cv2.resize(img, (width//resizeValue, height//resizeValue))
+
+    ### 建立比原圖大的底圖
+    imgMask = np.zeros((height//resizeValue, width//resizeValue + paddingValue, 3), dtype=np.uint8)
+    cv2.rectangle(imgMask, (0, 0), (width//resizeValue + paddingValue, height//resizeValue), (0, 0, 0), -1)
+    
+    ### 與原圖合併
+    imgMask[0:height//resizeValue, paddingValue//2:width//resizeValue + paddingValue//2] = img
 
     ### 將圖片轉換為灰度圖
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(imgMask, cv2.COLOR_BGR2GRAY)
 
-    ### 將灰度圖轉換為二進制圖像
-    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    # ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # ### 將灰度圖轉換為二進制圖像
+    # ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    ## 顯示圖片
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), thresh)
+    cv2.imshow('image', thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     ### 尋找輪廓
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -27,11 +41,18 @@ def mask(img):
     maxX, _, maxW, _ = cv2.boundingRect(max_contour)
 
     ###第一階段mask(矩形)
-    if maxX ==0:
-        img[0:height, maxW:width] = [0, 0, 0]
+    if maxX == paddingValue//2:
+        # imgMask[0:height//resizeValue, (maxW-paddingValue)*resizeValue:width//resizeValue] = [0, 255, 0]
+        imgMask[0:height//resizeValue, maxW:width//resizeValue + paddingValue//2] = [0, 255, 0]
     else:
-        img[0:height, 0: maxX] = [0, 0, 0]
+        imgMask[0:height//resizeValue, paddingValue//2: maxX] = [0, 255, 0]
 
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), imgMask)
+    cv2.imshow('image', imgMask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+
+    # if maxX !=paddingValue//2:
     ###過濾雜訊(鋁擠)
     for contour in contours:
         area = cv2.contourArea(contour)
@@ -40,38 +61,62 @@ def mask(img):
         else:
             if area != 0.0:
                 x, y, w, h = cv2.boundingRect(contour)
-                img[y:y+h, x:x+w] = [0, 0, 0]
+                imgMask[y:y+h, x:x+w] = [0, 0, 0]
+                # cv2.imshow('image', mask)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()
+    imgMask[thresh==0] = [0,0,0]
+
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), imgMask)
+    cv2.imshow('image', imgMask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
 
     ### 將圖片轉換為灰度圖(做細部調整)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(imgMask, cv2.COLOR_BGR2GRAY)
 
-    ### 將灰度圖轉換為二進制圖像
-    ret, thresh2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    ### 影像開運算
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    closeImg = cv2.morphologyEx(gray2, cv2.MORPH_OPEN, kernel)
+
+    ## 將灰度圖轉換為二進制圖像
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), closeImg)
+    cv2.imshow('gray', closeImg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    ret, thresh2 = cv2.threshold(closeImg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), thresh2)
+    cv2.imshow('image', thresh2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     ### 圖片最高/低點所有pixel
     topRow = thresh2[0, :]
-    btmRow = thresh2[height//4 -2, :]
+    btmRow = thresh2[height//resizeValue -2, :]
 
     ### 尋找門板最邊緣
-    if maxX !=0:
-        minTopKey = min(index for index, value in enumerate(topRow) if value == 255)
-        minBtmKey = min(index for index, value in enumerate(btmRow) if value == 255)
-        pts = np.array([[0, 0], [minTopKey*4, 0], [minBtmKey*4, height-1], [0, height-1]], np.int32)
+    if maxX !=paddingValue//2:
+        minTopKey = (min(index for index, value in enumerate(topRow) if value == 255) - paddingValue//2) * resizeValue
+        minBtmKey = (min(index for index, value in enumerate(btmRow) if value == 255) - paddingValue//2) * resizeValue
+        pts = np.array([[0, 0], [minTopKey, 0], [minBtmKey, height-1], [0, height-1]], np.int32)
     else:
-        maxTopKey = max(index for index, value in enumerate(topRow) if value == 255)
-        maxBtmKey = max(index for index, value in enumerate(btmRow) if value == 255)
-        pts = np.array([[maxTopKey*4, 0], [width, 0], [width, height-1], [maxBtmKey*4, height-1]], np.int32)
+        maxTopKey = (max(index for index, value in enumerate(topRow) if value == 255) - paddingValue//2) * resizeValue
+        maxBtmKey = (max(index for index, value in enumerate(btmRow) if value == 255) - paddingValue//2) * resizeValue
+        pts = np.array([[maxTopKey, 0], [width, 0], [width, height-1], [maxBtmKey, height-1]], np.int32)
 
     ###第二階段mask(梯形)
     cv2.fillPoly(imgOrgi, [pts], (0, 0, 0))
 
-    return imgOrgi
+    # return imgOrgi
 
     ###儲存圖片
-    # cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), imgOrgi)
+    cv2.imwrite('./data/mask/{}_mask.jpg'.format(fileName), imgOrgi)
 
-    ### 顯示圖片
-    # cv2.imshow('image', img)
+    # ### 顯示圖片
+    # cv2.imshow('image', imgOrgi)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
@@ -82,7 +127,7 @@ if __name__ == "__main__":
     for filename in os.listdir(folder_path):
         # 拼接文件路径
         name, extension = os.path.splitext(filename)
-        try:
-            mask(folder_path, name)
-        except:
-            print(name)
+        # try:
+        background(folder_path, name)
+        # except:
+        #     print(name)
